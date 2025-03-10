950a26908c7e7e066bdfa52d15f43f9806466cba
RGuzzy96 <ryanguzzwell@gmail.com>
2025-03-10 18:58:00 -0300

    initialized next app

---
+++ components/hi-mom.tsx
@@ -0,0 +1,65 @@
+'use client'
+
+import { useState } from "react";
+
+interface Sibling {
+    name: string;
+    type: 'sister' | 'brother'
+}
+
+const siblings: Sibling[] = [
+    {
+        name: 'Rick',
+        type: 'brother'
+    },
+    {
+        name: 'Brenda',
+        type: 'sister'
+    },
+    {
+        name: 'Rick',
+        type: 'brother'
+    },
+    {
+        name: 'Brenda',
+        type: 'sister'
+    }
+]
+
+export default function HiMomComponent({ momName }: { momName: string }){
+    const hasMomName = momName ? true : false;
+
+    const [dynamicSiblings, setDynamicSiblings] = useState<Sibling[]>([]);
+
+    console.log('Dynamic Siblings: ', dynamicSiblings);
+
+    const handleAddSibling = () => {
+        console.log('Adding sibling...')
+        setDynamicSiblings(prev => [...prev || [], siblings[2]]);
+    }
+
+    const removeSibling = (sib: Sibling) => {
+        const filteredSiblings = dynamicSiblings?.filter(sibling => sib.name !== sibling.name);
+        setDynamicSiblings(filteredSiblings);
+    }
+
+    return (
+        <div className="border border-white flex flex-col gap-y-3">
+            {hasMomName ? <p>{"Hi mom! mom name: " + momName}</p> : <p>Hi dad</p>}
+            {dynamicSiblings?.map((sibling, index) => {
+                return (
+                    <div onClick={() => removeSibling(sibling)} key={index}>
+                        {sibling.name}
+                    </div>
+                )
+            })}
+            <button 
+                onClick={handleAddSibling} 
+                className="border border-white px-3.5 py-2 rounded-md bg-neutral-900 hover:bg-neutral-800 cursor-pointer font-semibold"
+            >
+                Add sibling
+            </button>
+
+        </div>
+    )
+}
