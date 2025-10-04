7b22b66b8dde0ebb996d5d0c3dafce5a3e5429d4
RGuzzy96 <ryanguzzwell@gmail.com>
2025-10-04 14:45:18 -0300

    some example code

---
+++ components/platform/dashboard/dashboard-main.tsx
@@ -0,0 +1,20 @@
+import { Person } from "@/types/types"
+
+export default function DashboardMainComponent({
+    selected_person,
+    setSelectedPerson
+}: {
+    selected_person: Person | null,
+    setSelectedPerson: (person: Person | null) => void
+}){
+    return !selected_person ? (
+        <div>
+            Select a person
+        </div>
+    ) : (
+        <div className="flex flex-col gap-y-10 items-start bg-white text-gray-600">
+            {selected_person?.first_name} {selected_person?.last_name}
+            <button onClick={() => setSelectedPerson(null)}>Clear</button>
+        </div>
+    )
+}
